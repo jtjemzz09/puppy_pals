e76f0c54a3bd2112d2fc4a96c5656f0e15d9e0d8
jtjemzz09 <jtjemzz09@LAPTOP-292I2KQV>
2023-06-14 23:59:48 -0700

    added more functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,37 @@
 import {puppyList} from './data.js';
 import { useState } from 'react'
+import './App.css';
+
 
 
 function App() {
-
   const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
 
-  console.log("puppyList: ", puppyList);
+  function handleClick(puppy) {
+    setFeatPupId(puppy.id);
+  }
+
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
 
   return (
-    <div className="App">
-      {
-        puppies.map((puppy) => {
-             return <p key={puppy.id}>{puppy.name}</p>;
-           })
-       }
+    <div className="root">
+      {puppies.map((puppy) => {
+        return (
+          <p onClick={() => handleClick(puppy)} key={puppy.id}>
+            {puppy.name}
+          </p>
+        );
+      })}
+      {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
