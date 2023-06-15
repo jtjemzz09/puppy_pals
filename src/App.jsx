5605a8fb048f9ad9e147f31fcd71407f667237a4
jtjemzz09 <jtjemzz09@LAPTOP-292I2KQV>
2023-06-14 21:58:18 -0700

    import data.js to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
-import {puppyList} from './data.js'
+import {puppyList} from './data.js';
+import { useState } from 'react'
 
 
 function App() {
-console.log(puppyList)
-  return (
 
-      <div>
-       
-      </div>
-      
-  )
+  const [puppies, setPuppies] = useState(puppyList);
+
+  console.log("puppyList: ", puppyList);
+
+  return (
+    <div className="App">
+      {
+        puppies.map((puppy) => {
+             return <p key={puppy.id}>{puppy.name}</p>;
+           })
+       }
+    </div>
+  );
 }
 
-export default App
+export default App;
